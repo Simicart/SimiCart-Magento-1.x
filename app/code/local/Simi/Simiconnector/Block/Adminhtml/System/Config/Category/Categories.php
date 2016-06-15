be810e69835ae32d90916bbcb860495156254dd1
Cody Nguyễn <huyhai.nguyen@trueplus.vn>
2016-06-15 13:51:18 +0700

    categories

--- app/code/local/Simi/Simiconnector/Block/Adminhtml/System/Config/Category/Categories.php
+++ app/code/local/Simi/Simiconnector/Block/Adminhtml/System/Config/Category/Categories.php
@@ -1,19 +1,17 @@
 <?php
 
-class Simi_Simiconnector_Block_Adminhtml_System_Config_Category_Categories extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Categories
-{
-	protected $_categoryIds;
+class Simi_Simiconnector_Block_Adminhtml_System_Config_Category_Categories extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Categories {
+
+    protected $_categoryIds;
     protected $_selectedNodes = null;
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
-		$this->_withProductCount = false;
+        $this->_withProductCount = false;
         $this->setTemplate('simiconnector/categories.phtml');
     }
-	
-	public function getCategoryCollection()
-    {
+
+    public function getCategoryCollection() {
         $storeId = $this->getRequest()->getParam('store', $this->_getDefaultStoreId());
         $collection = $this->getData('category_collection');
         if (is_null($collection)) {
@@ -21,10 +19,10 @@ class Simi_Simiconnector_Block_Adminhtml_System_Config_Category_Categories exten
 
             /* @var $collection Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection */
             $collection->addAttributeToSelect('name')
-                ->addAttributeToSelect('is_active')
-                ->setProductStoreId($storeId)
-                ->setLoadProductCount($this->_withProductCount)
-                ->setStoreId($storeId);
+                    ->addAttributeToSelect('is_active')
+                    ->setProductStoreId($storeId)
+                    ->setLoadProductCount($this->_withProductCount)
+                    ->setStoreId($storeId);
 
             $this->setData('category_collection', $collection);
         }
@@ -36,28 +34,24 @@ class Simi_Simiconnector_Block_Adminhtml_System_Config_Category_Categories exten
      *
      * @return boolean
      */
-    public function isReadonly()
-    {
-        return false;//$this->getProduct()->getCategoriesReadonly();
+    public function isReadonly() {
+        return false; //$this->getProduct()->getCategoriesReadonly();
     }
 
-    protected function getCategoryIds()
-    {
-        return explode(',',$this->getIdsString());//$this->getProduct()->getCategoryIds();
+    protected function getCategoryIds() {
+        return explode(',', $this->getIdsString()); //$this->getProduct()->getCategoryIds();
     }
 
-    public function getIdsString()
-    {		
-        return Mage::getStoreConfig("simiconnector/general/categories_in_app");//Mage::registry('simiconnector_categories');//Mage::registry('bannerslider_data')->getCategories();//implode(',', $this->getCategoryIds());
+    public function getIdsString() {
+        return Mage::getStoreConfig("simiconnector/general/categories_in_app"); //Mage::registry('simiconnector_categories');//Mage::registry('bannerslider_data')->getCategories();//implode(',', $this->getCategoryIds());
     }
-    public function getLoadTreeUrl($expanded=null)
-    {
-        $params = array('_current'=>true, 'id'=>null,'store'=>null);
-        if (
-            (is_null($expanded) && Mage::getSingleton('admin/session')->getIsTreeWasExpanded())
-            || $expanded == true) {
+
+    public function getLoadTreeUrl($expanded = null) {
+        $params = array('_current' => true, 'id' => null, 'store' => null);
+        if ((is_null($expanded) && Mage::getSingleton('admin/session')->getIsTreeWasExpanded()) || $expanded == true) {
             $params['expand_all'] = true;
         }
         return $this->getUrl('adminhtml/simiconnector_config/categoriesJson', $params);
     }
+
 }
--- app/code/local/Simi/Simiconnector/Model/Api/Categories.php
+++ app/code/local/Simi/Simiconnector/Model/Api/Categories.php
@@ -6,11 +6,39 @@
  * Date: 5/19/2016
  * Time: 4:47 PM
  */
-class Simi_Simiconnector_Model_Api_Categories extends Simi_Simiconnector_Model_Api_Abstract
-{
+class Simi_Simiconnector_Model_Api_Categories extends Simi_Simiconnector_Model_Api_Abstract {
 
-    public function setBuilderQuery()
-    {
-        Zend_debug::dump(Mage::getStoreConfig("simiconnector/general/categories_in_app"));die;
+    protected $_DEFAULT_ORDER = 'position';
+    protected $_visible_array;
+
+    public function setBuilderQuery() {
+        $data = $this->getData();
+        if (!$data['resourceid']) {
+            $data['resourceid'] = Mage::app()->getStore($storeId)->getRootCategoryId();
+        }
+        if (Mage::getStoreConfig('simiconnector/general/categories_in_app'))
+            $this->_visible_array = explode(',', Mage::getStoreConfig('simiconnector/general/categories_in_app'));
+        $this->builderQuery = Mage::getModel('catalog/category')->getCollection()->addFieldToFilter('parent_id', $data['resourceid'])->addAttributeToSelect('*');
+        if ($this->_visible_array)
+            $this->builderQuery->addFieldToFilter('entity_id', array('in' => $this->_visible_array));
     }
+
+    public function index() {
+        $result = parent::index();
+        foreach ($result['categories'] as $index => $catData) {
+            $childCollection = Mage::getModel('catalog/category')->getCollection()->addFieldToFilter('parent_id', $catData['entity_id']);
+            if ($this->_visible_array)
+                $childCollection->addFieldToFilter('entity_id', array('in' => $this->_visible_array));
+            if ($childCollection->count() > 0)
+                $result['categories'][$index]['has_children'] = TRUE;
+            else
+                $result['categories'][$index]['has_children'] = FALSE;
+        }
+        return $result;
+    }
+
+    public function show() {
+        return $this->index();
+    }
+
 }
