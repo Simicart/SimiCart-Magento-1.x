93b555aeb8ef73cf3fa1eecfb93dac83cbd47684
Cody Nguyễn <huyhai.nguyen@trueplus.vn>
2016-06-22 09:31:11 +0700

    QR Bar codes

--- app/code/local/Simi/Simiconnector/Model/Api/Simibarcodes.php
+++ app/code/local/Simi/Simiconnector/Model/Api/Simibarcodes.php
@@ -9,17 +9,18 @@
 class Simi_Simiconnector_Model_Api_Simibarcodes extends Simi_Simiconnector_Model_Api_Abstract {
 
     protected $_DEFAULT_ORDER = 'barcode_id';
-   
+
     public function setBuilderQuery() {
         $data = $this->getData();
         if ($data['resourceid']) {
-            $this->setStoreView($data);
-            $this->setCurrency($data);
-            $this->builderQuery = Mage::getModel('simiconnector/simibarcode')->load($data['resourceid']);
+            $this->builderQuery = Mage::getModel('simiconnector/simibarcode')->getCollection()->addFieldToFilter('barcode_status', '1')->addFieldToFilter('barcode', $data['resourceid'])->getFirstItem();
+            if (!$this->builderQuery->getId())
+                $this->builderQuery = Mage::getModel('simiconnector/simibarcode')->getCollection()->addFieldToFilter('barcode_status', '1')->addFieldToFilter('qrcode', $data['resourceid'])->getFirstItem();
+            if (!$this->builderQuery->getId())
+                throw new Exception(Mage::helper('catalog')->__('There is No Product Matchs your Code'), 4);
         } else {
             $this->builderQuery = Mage::getModel('simiconnector/simibarcode')->getCollection();
         }
     }
-
 
 }
