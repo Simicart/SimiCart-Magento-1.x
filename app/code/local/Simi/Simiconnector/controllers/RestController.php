390c3e9bd19a17167a2dda9d3d0b3d6f7d08cf1d
Max <hoanghai.ta@trueplus.vn>
2016-08-08 12:02:13 +0700

    Max update

--- app/code/local/Simi/Simiconnector/controllers/RestController.php
+++ app/code/local/Simi/Simiconnector/controllers/RestController.php
@@ -8,19 +8,32 @@
  */
 class Simi_Simiconnector_RestController extends Simi_Simiconnector_Controller_Action
 {
-    public function v2Action(){
+    public function v2Action()
+    {
         ob_start();
-        try{
+        try {
             $result = $this->_getServer()
                 ->init($this)->run();
             $this->_printData($result);
-        }catch (Exception $e){
+        } catch (Exception $e) {
+            $results = array();
             $result = array();
-            $result['error'] = array(
-                'code' => $e->getCode(),
-                'message'=> $e->getMessage(),
-            );
-            $this->_printData($result);
+            if (is_array($e->getMessage())) {
+                $messages = $e->getMessage();
+                foreach ($messages as $message) {
+                    $result[] = array(
+                        'code' => $e->getCode(),
+                        'message' => $message,
+                    );
+                }
+            } else {
+                $result[] = array(
+                    'code' => $e->getCode(),
+                    'message' => $e->getMessage(),
+                );
+            }
+            $results['errors'] = $result;
+            $this->_printData($results);
         }
         exit();
         ob_end_flush();
