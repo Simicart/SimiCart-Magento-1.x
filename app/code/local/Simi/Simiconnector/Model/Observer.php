62ea076fc6734b2bcef52c0884483c6a757efa53
Cody Nguyễn <huyhai.nguyen@trueplus.vn>
2016-09-13 15:39:34 +0700

    cody

--- app/code/local/Simi/Simiconnector/Model/Observer.php
+++ app/code/local/Simi/Simiconnector/Model/Observer.php
@@ -29,103 +29,106 @@ class Simi_Simiconnector_Model_Observer {
      */
     public function sendNotificationProductChangePrice($observer) {
         $helper = Mage::helper('simiconnector/siminotification');
-        if ($helper->getConfig('noti_price_enable')) {
-            $newProduct = $observer->getProduct();
-            $newPrice = $newProduct->getData('price');
-            $newSpecialPrice = $newProduct->getData('special_price');
-            $oldProduct = Mage::getModel('catalog/product')->load($newProduct->getId());
-            $oldPrice = $oldProduct->getData('price');
-            $oldSpecialPrice = $oldProduct->getData('special_price');
-            if ($oldSpecialPrice != $newSpecialPrice && $newProduct->getId() > 0 && $newProduct->getStatus() == '1' && $newProduct->getVisibility() != '1') {
-                $data = array();
-                $content = Mage::helper('simiconnector/siminotification')->__(
-                        $helper->getConfig('noti_price_message'), $newProduct->getName(), $this->formatPrice($oldSpecialPrice), $this->formatPrice($newSpecialPrice));
-                $data['website_id'] = $helper->getConfig('noti_price_website');
-                $data['show_popup'] = $helper->getConfig('noti_price_showpopup');
-                $data['notice_title'] = $helper->getConfig('noti_price_title');
-                $data['notice_url'] = $helper->getConfig('noti_price_url');
-                $data['notice_content'] = $content;
-                $data['device_id'] = $helper->getConfig('noti_price_platform');
-                $data['notice_sanbox'] = $helper->getConfig('noti_price_sandbox');
-                $data['type'] = $helper->getConfig('noti_price_type');
-                $data['product_id'] = $newProduct->getId();
-                $data['category_id'] = $helper->getConfig('noti_price_category_id');
-                $data['category_name'] = $this->getCategoryName($helper->getConfig('noti_price_category_id'));
-                $data['has_child'] = $this->getCategoryChildrenCount($helper->getConfig('noti_price_category_id'));
-                $data['created_time'] = now();
-                $data['notice_type'] = 1;
-                $data['notice_sanbox'] = '2';
-                $storeViewCollection = Mage::getModel('core/store')->getCollection();
-                foreach($storeViewCollection as $storeview) {
-                    $data['storeview_id'] = $storeview->getId();
-                    $data['devices_pushed'] = $this->getAllDeviceToPush($storeview->getId());
+        $storeViewCollection = Mage::getModel('core/store')->getCollection();
+        foreach ($storeViewCollection as $storeview) {
+            $storeviewId = $storeview->getId();
+            if (Mage::getStoreConfig('simiconnector/notification/noti_price_enable', $storeviewId)) {
+                $newProduct = $observer->getProduct();
+                if (!in_array($storeview->getWebsiteId(), $newProduct->getWebsiteIds()))
+                    continue;
+                $newPrice = $newProduct->getData('price');
+                $newSpecialPrice = $newProduct->getData('special_price');
+                $oldProduct = Mage::getModel('catalog/product')->load($newProduct->getId());
+                $oldPrice = $oldProduct->getData('price');
+                $oldSpecialPrice = $oldProduct->getData('special_price');
+                if ($oldSpecialPrice != $newSpecialPrice && $newProduct->getId() > 0 && $newProduct->getStatus() == '1' && $newProduct->getVisibility() != '1') {
+                    $data = array();
+                    $content = Mage::helper('simiconnector/siminotification')->__(
+                            $helper->getConfig('noti_price_message'), $newProduct->getName(), $this->formatPrice($oldSpecialPrice), $this->formatPrice($newSpecialPrice));
+                    $data['website_id'] = $helper->getConfig('noti_price_website');
+                    $data['show_popup'] = $helper->getConfig('noti_price_showpopup');
+                    $data['notice_title'] = $helper->getConfig('noti_price_title');
+                    $data['notice_url'] = $helper->getConfig('noti_price_url');
+                    $data['notice_content'] = $content;
+                    $data['device_id'] = $helper->getConfig('noti_price_platform');
+                    $data['notice_sanbox'] = $helper->getConfig('noti_price_sandbox');
+                    $data['type'] = $helper->getConfig('noti_price_type');
+                    $data['product_id'] = $newProduct->getId();
+                    $data['category_id'] = $helper->getConfig('noti_price_category_id');
+                    $data['category_name'] = $this->getCategoryName($helper->getConfig('noti_price_category_id'));
+                    $data['has_child'] = $this->getCategoryChildrenCount($helper->getConfig('noti_price_category_id'));
+                    $data['created_time'] = now();
+                    $data['notice_type'] = 1;
+                    $data['notice_sanbox'] = '2';
+                    $data['storeview_id'] = $storeviewId;
+                    $data['devices_pushed'] = $this->getAllDeviceToPush($storeviewId);
                     if ($data['devices_pushed']) {
                         Mage::helper('simiconnector/siminotification')->sendNotice($data);
                     }
-                }
-            } elseif ($oldPrice != $newPrice && $newProduct->getId() > 0 && $newProduct->getStatus() == '1' && $newProduct->getVisibility() != '1') {
-                $data = array();
-                $content = Mage::helper('simiconnector/siminotification')->__(
-                        $helper->getConfig('noti_price_message'), $newProduct->getName(), $this->formatPrice($oldPrice), $this->formatPrice($newPrice));
-                $data['website_id'] = $helper->getConfig('noti_price_website');
-                $data['show_popup'] = $helper->getConfig('noti_price_showpopup');
-                $data['notice_title'] = $helper->getConfig('noti_price_title');
-                $data['notice_url'] = $helper->getConfig('noti_price_url');
-                $data['notice_content'] = $content;
-                $data['device_id'] = $helper->getConfig('noti_price_platform');
-                $data['notice_sanbox'] = $helper->getConfig('noti_price_sandbox');
-                $data['type'] = $helper->getConfig('noti_price_type');
-                $data['product_id'] = $newProduct->getId();
-                $data['category_id'] = $helper->getConfig('noti_price_category_id');
-                $data['category_name'] = $this->getCategoryName($helper->getConfig('noti_price_category_id'));
-                $data['has_child'] = $this->getCategoryChildrenCount($helper->getConfig('noti_price_category_id'));
-                $data['created_time'] = now();
-                $data['notice_type'] = 1;
-                $data['notice_sanbox'] = '2';
-                $storeViewCollection = Mage::getModel('core/store')->getCollection();
-                foreach($storeViewCollection as $storeview) {
-                    $data['storeview_id'] = $storeview->getId();
-                    $data['devices_pushed'] = $this->getAllDeviceToPush($storeview->getId());
+                } elseif ($oldPrice != $newPrice && $newProduct->getId() > 0 && $newProduct->getStatus() == '1' && $newProduct->getVisibility() != '1') {
+                    $data = array();
+                    $content = Mage::helper('simiconnector/siminotification')->__(
+                            $helper->getConfig('noti_price_message'), $newProduct->getName(), $this->formatPrice($oldPrice), $this->formatPrice($newPrice));
+                    $data['website_id'] = $helper->getConfig('noti_price_website');
+                    $data['show_popup'] = $helper->getConfig('noti_price_showpopup');
+                    $data['notice_title'] = $helper->getConfig('noti_price_title');
+                    $data['notice_url'] = $helper->getConfig('noti_price_url');
+                    $data['notice_content'] = $content;
+                    $data['device_id'] = $helper->getConfig('noti_price_platform');
+                    $data['notice_sanbox'] = $helper->getConfig('noti_price_sandbox');
+                    $data['type'] = $helper->getConfig('noti_price_type');
+                    $data['product_id'] = $newProduct->getId();
+                    $data['category_id'] = $helper->getConfig('noti_price_category_id');
+                    $data['category_name'] = $this->getCategoryName($helper->getConfig('noti_price_category_id'));
+                    $data['has_child'] = $this->getCategoryChildrenCount($helper->getConfig('noti_price_category_id'));
+                    $data['created_time'] = now();
+                    $data['notice_type'] = 1;
+                    $data['notice_sanbox'] = '2';
+                    $data['storeview_id'] = $storeviewId;
+                    $data['devices_pushed'] = $this->getAllDeviceToPush($storeviewId);
                     if ($data['devices_pushed']) {
                         Mage::helper('simiconnector/siminotification')->sendNotice($data);
                     }
+                } elseif (!$newProduct->getId()) {
+                    Mage::getSingleton('core/session')->setData('new_added_product_sku', $newProduct->getSku());
                 }
-            } elseif (!$newProduct->getId()) {
-                Mage::getSingleton('core/session')->setData('new_added_product_sku', $newProduct->getSku());
             }
         }
     }
 
     public function sendNotificationNewProduct($observer) {
         $helper = Mage::helper('simiconnector/siminotification');
-        if ($helper->getConfig('new_product_enable')) {
-            $newProduct = $observer->getProduct();
-            $lastProductId = Mage::getModel('catalog/product')->getCollection()
-                            ->setOrder('entity_id', 'desc')->getFirstItem()->getId();
-            if ($newProduct->getId() && $newProduct->getId() == $lastProductId && $newProduct->getStatus() == '1' && $newProduct->getVisibility() != '1' && $newProduct->getSku() == Mage::getSingleton('core/session')->getData('new_added_product_sku')) {
-                $content = Mage::helper('simiconnector/siminotification')->__(
-                        $helper->getConfig('new_product_message'), $newProduct->getName());
-                $data = array();
-                $data['website_id'] = $helper->getConfig('new_product_website');
-                $data['show_popup'] = $helper->getConfig('new_product_showpopup');
-                $data['notice_title'] = $helper->getConfig('new_product_title');
-                $data['notice_url'] = $helper->getConfig('new_product_url');
-                $data['notice_content'] = $content;
-                $data['device_id'] = $helper->getConfig('new_product_platform');
-                $data['notice_sanbox'] = $helper->getConfig('new_product_sandbox');
-                $data['type'] = $helper->getConfig('new_product_type');
-                $data['product_id'] = $newProduct->getId();
-                $data['category_id'] = $helper->getConfig('new_product_category_id');
-                $data['category_name'] = $this->getCategoryName($helper->getConfig('new_product_category_id'));
-                $data['has_child'] = $this->getCategoryChildrenCount($helper->getConfig('new_product_category_id'));
-                $data['created_time'] = now();
-                $data['notice_type'] = 2;
-                $data['notice_sanbox'] = '2';
-                Mage::getSingleton('core/session')->setData('new_added_product_sku', NULL);
-                $storeViewCollection = Mage::getModel('core/store')->getCollection();
-                foreach($storeViewCollection as $storeview) {
-                    $data['storeview_id'] = $storeview->getId();
-                    $data['devices_pushed'] = $this->getAllDeviceToPush($storeview->getId());
+        $storeViewCollection = Mage::getModel('core/store')->getCollection();
+        foreach ($storeViewCollection as $storeview) {
+            $storeviewId = $storeview->getId();
+            if ($helper->getConfig('new_product_enable')) {
+                $newProduct = $observer->getProduct();
+                if (!in_array($storeview->getWebsiteId(), $newProduct->getWebsiteIds()))
+                    continue;
+                $lastProductId = Mage::getModel('catalog/product')->getCollection()
+                                ->setOrder('entity_id', 'desc')->getFirstItem()->getId();
+                if ($newProduct->getId() && $newProduct->getId() == $lastProductId && $newProduct->getStatus() == '1' && $newProduct->getVisibility() != '1' && $newProduct->getSku() == Mage::getSingleton('core/session')->getData('new_added_product_sku')) {
+                    $content = Mage::helper('simiconnector/siminotification')->__(
+                            $helper->getConfig('new_product_message'), $newProduct->getName());
+                    $data = array();
+                    $data['website_id'] = $helper->getConfig('new_product_website');
+                    $data['show_popup'] = $helper->getConfig('new_product_showpopup');
+                    $data['notice_title'] = $helper->getConfig('new_product_title');
+                    $data['notice_url'] = $helper->getConfig('new_product_url');
+                    $data['notice_content'] = $content;
+                    $data['device_id'] = $helper->getConfig('new_product_platform');
+                    $data['notice_sanbox'] = $helper->getConfig('new_product_sandbox');
+                    $data['type'] = $helper->getConfig('new_product_type');
+                    $data['product_id'] = $newProduct->getId();
+                    $data['category_id'] = $helper->getConfig('new_product_category_id');
+                    $data['category_name'] = $this->getCategoryName($helper->getConfig('new_product_category_id'));
+                    $data['has_child'] = $this->getCategoryChildrenCount($helper->getConfig('new_product_category_id'));
+                    $data['created_time'] = now();
+                    $data['notice_type'] = 2;
+                    $data['notice_sanbox'] = '2';
+                    Mage::getSingleton('core/session')->setData('new_added_product_sku', NULL);
+                    $data['storeview_id'] = $storeviewId;
+                    $data['devices_pushed'] = $this->getAllDeviceToPush($storeviewId);
                     if ($data['devices_pushed']) {
                         Mage::helper('simiconnector/siminotification')->sendNotice($data);
                     }
@@ -157,7 +160,7 @@ class Simi_Simiconnector_Model_Observer {
     public function getAllDeviceToPush($storeview_id) {
         $idArray = array();
         $tokenArray = array();
-        foreach (Mage::getModel('simiconnector/device')->getCollection()->addFieldToFilter('storeview_id',$storeview_id) as $device) {
+        foreach (Mage::getModel('simiconnector/device')->getCollection()->addFieldToFilter('storeview_id', $storeview_id) as $device) {
             if (!in_array($device->getData('device_token'), $idArray)) {
                 $idArray[] = $device->getId();
                 $tokenArray[] = $device->getData('device_token');
