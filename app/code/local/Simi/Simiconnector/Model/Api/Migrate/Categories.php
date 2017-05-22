c070df9507ad14250b88425df4fe6172245a37b8
Cody Nguyen <cody@simicart.com>
2017-05-22 09:11:55 +0700

    update migrate

--- app/code/local/Simi/Simiconnector/Model/Api/Migrate/Categories.php
+++ app/code/local/Simi/Simiconnector/Model/Api/Migrate/Categories.php
@@ -12,4 +12,30 @@ class Simi_Simiconnector_Model_Api_Migrate_Categories extends Simi_Simiconnector
             $this->builderQuery = Mage::getModel('catalog/category')->load($data['resourceid']);
         }
     }
+
+    public function index()
+    {
+        $result = parent::index();
+        $storeIds = $this->getAllStoreIds();
+        foreach ($result['migrate_categories'] as $index=>$category) {
+            $nameArray = array();
+            foreach ($storeIds as $storeId) {
+                $nameByStore = Mage::getModel('catalog/category')
+                    ->setStoreId($storeId)->load($category['entity_id'])->getName();
+                if ($nameByStore) {
+                    $nameArray[] = array($storeId, $nameByStore);
+                }
+            }
+            if (count($nameArray)) {
+                $result['migrate_categories'][$index]['json_name'] = json_encode($nameArray);
+            }
+        }
+        return $result;
+    }
+
+    public function getAllStoreIds()
+    {
+        return Mage::getModel('core/store')
+            ->getCollection()->getAllIds();
+    }
 }
