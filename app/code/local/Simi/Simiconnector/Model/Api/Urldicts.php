74331c2099dae69e252998f56260b6b399c4d6d8
Cody Nguyen <cody@simicart.com>
2018-08-13 15:28:24 +0700

    Update url dict api

--- app/code/local/Simi/Simiconnector/Model/Api/Urldicts.php
+++ app/code/local/Simi/Simiconnector/Model/Api/Urldicts.php
@@ -25,20 +25,24 @@ class Simi_Simiconnector_Model_Api_Urldicts extends Simi_Simiconnector_Model_Api
             $apiModel->setBuilderQuery();
             $result['urldict']['simi_product_data'] = $apiModel->show();
         } else if(isset($result['urldict']['category_id']) && $result['urldict']['category_id']) {
-            $apiModel = Mage::getModel('simiconnector/api_categories');
-            $result['urldict']['simi_catetory_name'] = Mage::getModel('catalog/category')
-                ->load($result['urldict']['category_id'])
-                ->getName();
-            $data['resourceid'] = $result['urldict']['category_id'];
-            $apiModel->pluralKey = 'categories';
-            $apiModel->singularKey = 'category';
-            $apiModel->setData($data);
-            $apiModel->setBuilderQuery();
-            $result['urldict']['simi_category_child'] = $apiModel->show();
-            
+            if (isset($data['params']['get_child_cat']) && $data['params']['get_child_cat']) {
+                $apiModel = Mage::getModel('simiconnector/api_categories');
+                $result['urldict']['simi_catetory_name'] = Mage::getModel('catalog/category')
+                    ->load($result['urldict']['category_id'])
+                    ->getName();
+                $data['resourceid'] = $result['urldict']['category_id'];
+                $apiModel->pluralKey = 'categories';
+                $apiModel->singularKey = 'category';
+                $apiModel->setData($data);
+                $apiModel->setBuilderQuery();
+                $result['urldict']['simi_category_child'] = $apiModel->show();
+            }
+
             $productListModel = Mage::getModel('simiconnector/api_products');
             unset($data['resourceid']);
             $data['params'][self::FILTER] = array('cat_id'=>$result['urldict']['category_id']);
+            $data['params']['image_width'] = isset($data['params']['image_width'])?$data['params']['image_width']:200;
+            $data['params']['image_height'] = isset($data['params']['image_height'])?$data['params']['image_height']:200;
             $data['params']['limit'] = 12;
             $productListModel->pluralKey = 'products';
             $productListModel->singularKey = 'product';
